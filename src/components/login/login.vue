59c2d890d723c9b4c4c48cd37933e8eff817f4b8
iyeix <2213703737@qq.com>
2020-04-04 17:12:34 +0800

    登录成功引入home组件

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -36,7 +36,7 @@ export default {
           meta: { msg, status }
         } = res.data
         if (status === 200) {
-        //   this.$router.push({name: 'home'})
+          this.$router.push({name: 'home'})
           this.$message.success(msg)
         } else {
           this.$message.warning(msg)
