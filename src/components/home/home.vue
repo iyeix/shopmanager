a5db2cc3ff9840d0c3a37d63d20c5e087536ee1d
iyeix <2213703737@qq.com>
2020-04-05 15:42:31 +0800

    头部样式调整

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,6 +1,17 @@
 <template>
   <el-container class="container">
-    <el-header class="header">Header</el-header>
+    <el-header class="header">
+      <el-row>
+        <el-col :span="22" class="head">
+            <h3>电商后台管理系统</h3>
+        </el-col>
+        <el-col :span="2">
+          <div class="grid-content bg-purple">
+              <a href="#" class="loginout">退出</a>
+          </div>
+        </el-col>
+      </el-row>
+    </el-header>
     <el-container>
       <el-aside class="aside" width="200px">Aside</el-aside>
       <el-main class="main">Main</el-main>
@@ -13,17 +24,22 @@ export default {}
 </script>
 
 <style>
-.container{
-    height: 100%;
+.container {
+  height: 100%;
 }
-.header{
-    background-color: #b3c0d1;
+.header {
+  background-color: #b3c0d1;
 }
-.aside{
-    background-color: #d3dce6;
+.aside {
+  background-color: #d3dce6;
 }
-.main{
-    background-color: #e9eef3;
-    height: 100%;
+.main {
+  background-color: #e9eef3;
+  height: 100%;
+}
+/* 头部 */
+.loginout{
+    text-decoration: none;
+    line-height: 60px;
 }
 </style>
