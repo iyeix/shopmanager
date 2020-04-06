95c1bb07cbf843a4e80bd2bec83cda89e0eba5e0
iyeix <2213703737@qq.com>
2020-04-06 16:11:58 +0800

    用户列表面包屑和搜索框

--- src/components/users/users.vue
+++ src/components/users/users.vue
@@ -1,9 +1,43 @@
 <template>
-  <div>users</div>
+  <el-card class="box-card">
+    <!-- 面包屑 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{path: '/'}">首页</el-breadcrumb-item>
+      <el-breadcrumb-item :to="{path: '/'}">用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item :to="{path: 'users'}">用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 搜索 -->
+    <el-row class="searchRow">
+      <el-col>
+        <el-input placeholder="请输入内容" v-model="query" class="inputSearch">
+          <el-button slot="append" icon="el-icon-search"></el-button>
+        </el-input>
+        <el-button type="success" plain>添加用户</el-button>
+      </el-col>
+    </el-row>
+    <!-- 表格 -->
+    <!-- 分页 -->
+  </el-card>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      query: ''
+    }
+  }
+}
 </script>
 
-<style></style>
+<style>
+.box-card {
+  height: 100%;
+}
+.searchRow{
+  margin-top: 20px;
+}
+.inputSearch{
+  width: 300px;
+}
+</style>
